eeab49d47936b819e7d4fffbbca93905de93a271
Budi Santoso <budi.santoso290103@gmail.com>
2026-01-27 09:13:35 +0700

    app provide untuk https dan auth beserta id version

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check-access' => \App\Http\Middleware\MultiAuthUser::class,
+        'admin.auth' => \App\Http\Middleware\AdminAuth::class,
         'pegawai.auth' => \App\Http\Middleware\PegawaiAuth::class,
-        'admin.auth'   => \App\Http\Middleware\AdminAuth::class,
     ];
 }
