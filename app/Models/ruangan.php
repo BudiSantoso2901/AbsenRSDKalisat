0200dca5d1a1601d0e3aec766291152765916ac7
Budi Santoso <budi.santoso290103@gmail.com>
2026-04-08 17:49:20 +0700

    perbaikan model

--- app/Models/ruangan.php
+++ app/Models/ruangan.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ruangan extends Model
+class Ruangan extends Model
 {
     use HasFactory;
-    protected $table = 'ruangans';
+    protected $table = 'Ruangans';
     protected $fillable = [
         'nama_ruangan',
     ];
