4d36d7c87077faf90235234e1c258b01eb3b3c7c
Budi Santoso <budi.santoso290103@gmail.com>
2026-04-20 10:50:33 +0700

    CSRF TOKEN LOGIn

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="manifest" href="/manifest.json">
     <meta name="theme-color" content="#28a745">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="apple-touch-icon" href="/icon/icon-192.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
