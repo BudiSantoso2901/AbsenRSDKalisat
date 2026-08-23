46105dd926641e9641064a520d160388ded30072
VisualMarcell <marcellkusuma2609@gmail.com>
2026-08-23 21:16:53 +0700

    responsive fix

--- resources/views/Pegawai/Panduan.blade.php
+++ resources/views/Pegawai/Panduan.blade.php
@@ -5,14 +5,28 @@
 <div class="container-xxl flex-grow-1 container-p-y">
 
     <iframe
+        class="scribe-frame"
         src="https://scribehow.com/embed/Panduan_Absensi_dan_Pengaturan_Profil_Pegawai_RSD_Kalisat__Cw9PX8LeQsaL2rsTOcYazA?as=scrollable"
-        width="100%"
-        height="800"
         title="Panduan Absensi dan Pengaturan Profil Pegawai RSD Kalisat"
-        allowfullscreen
-        style="border:0; min-height:640px;">
+        allowfullscreen>
     </iframe>
 
 </div>
+
+<style>
+.scribe-frame{
+    width:100%;
+    height:800px;
+    min-height:640px;
+    border:0;
+}
+
+@media(max-width:767px){
+    .scribe-frame{
+        height:calc(100dvh - 120px);
+        min-height:600px;
+    }
+}
+</style>
 
 @endsection
