7a82d26f561be2ada6aea4345d647fac4a8a3087
VisualMarcell <marcellkusuma2609@gmail.com>
2026-08-23 21:16:36 +0700

    Panduan Book baru dan responsive

--- resources/views/Pegawai/Panduan.blade.php
+++ resources/views/Pegawai/Panduan.blade.php
@@ -1,11 +1,18 @@
 @extends('_layouts.layouts')
 
 @section('content')
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <div class="iframe-container" style="position: relative; width: 100%; padding-bottom: 84.875%;">
-            <iframe
-                src="https://scribehow.com/embed/How_to_Change_Your_Shift_and_Clock_In__nB_YywikQLyUbi5D4hKyXA?as=scrollable"
-                width="100%" height="800" allow="fullscreen" style="border: 0; min-height: 640px"></iframe>
-        </div>
-    </div>
+
+<div class="container-xxl flex-grow-1 container-p-y">
+
+    <iframe
+        src="https://scribehow.com/embed/Panduan_Absensi_dan_Pengaturan_Profil_Pegawai_RSD_Kalisat__Cw9PX8LeQsaL2rsTOcYazA?as=scrollable"
+        width="100%"
+        height="800"
+        title="Panduan Absensi dan Pengaturan Profil Pegawai RSD Kalisat"
+        allowfullscreen
+        style="border:0; min-height:640px;">
+    </iframe>
+
+</div>
+
 @endsection
